acd884f2308f9140a306dbab4fea586a4ce9838a
swapnil <130269745+swapnil7620@users.noreply.github.com>
2026-05-22 16:39:41 +0530

    count vowels from string

---
+++ Python Code/CountVowels.py
@@ -0,0 +1,13 @@
+
+sentence = "Apple".lower()
+counter =0;
+for i in range(len(sentence)):
+    if("aeioi".find(sentence[i]) !=-1):
+        counter +=1;
+ 
+
+print(counter)
+
+
+   
+# print(sentence)
