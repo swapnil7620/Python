8af673653c0d03e22955b13fe4bc2537798a66f0
swapnil <130269745+swapnil7620@users.noreply.github.com>
2026-05-28 15:27:03 +0530

    How to take input from user

---
+++ Python Code/Input.py
@@ -0,0 +1,10 @@
+
+# By defalut it will take as a Object and you need to convert into required type
+name = input("Enter the name ");
+marks =  int(input("Enter the marks "));
+
+print("Name :"+ name);
+
+print("Marks :", marks)
+
+
